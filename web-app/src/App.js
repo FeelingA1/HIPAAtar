510e864bd90c6d1fafe313315711825fe60803e0
Sujitt <sujitt.rameshkumar@gmail.com>
2022-02-28 18:51:29 -0500

    Navbar working

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar"
+import Goals from "./pages/Goals"
+import Messages from "./pages/Messages"
+import Portfolio from "./pages/Portfolio"
+import Quizzes from "./pages/Quizzes"
+import Tips from "./pages/Tips"
+import Groups from "./pages/Groups"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='' element={<Portfolio/>}></Route>
+        <Route path='/groups' element={<Groups/>}></Route>
+        <Route path='/goals' element={<Goals/>}></Route>
+        <Route path='/messages' element={<Messages/>}></Route>
+        <Route path='/quizzes' element={<Quizzes/>}></Route>
+        <Route path='/tips' element={<Tips/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ web-app/src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { Button } from './Button';
+import './Navbar.css';
+
+const Navbar= () =>{
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+      if (window.innerWidth <= 960) {
+          setButton(false)
+      } else {
+          setButton(true);
+      }
+  };
+
+  useEffect(() => {
+      showButton()
+  }, []);
+
+    return (
+      <nav className="navbar">
+        <div className="navbar-container">
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to="/" className='nav-links'>Portfolio</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to="/groups" className='nav-links'>My Groups</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to="/goals" className='nav-links'>My Goals</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to="/messages" className='nav-links'>Messages</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to="/quizzes" className='nav-links'>Quizzes</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to="/tips" className='nav-links'>Tips</Link>
+          </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+  export default Navbar;

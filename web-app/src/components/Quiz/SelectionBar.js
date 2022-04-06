f8a66063a07d04f35dd1698cc5bb6a11a0b54c59
FeelingA1 <50811560+FeelingA1@users.noreply.github.com>
2022-04-06 14:01:37 -0400

    Remove unnecessary comments

--- web-app/src/components/Quiz/SelectionBar.js
+++ web-app/src/components/Quiz/SelectionBar.js
@@ -1,12 +1,6 @@
 import React, {useState} from "react";
 
 const SelectionBar = ({setQuiz1, setQuiz2, setQuiz3}) => {
-    /* var [quizNum, setQuizNum] = useState(quizNumber)
-    console.log('beginning ' + quizNum)
-
-    const handleQuizSelector = (num) => {
-        setQuizNum(num)
-    } */
     return (
         <div className="QuizSelection">
             <button className="QuizSelectionButton" onClick={setQuiz1}>quiz 1</button>
